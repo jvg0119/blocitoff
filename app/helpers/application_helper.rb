61f010f074326550d45874cb9b54ae1732fb5a85
Joseph <jvg_0119@yahoo.com>
2016-12-15 12:38:06 -0800

    user sign up, in and out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def class_type(index)
+	 case index 
+	      when "success" then "alert alert-success" # green
+	      when "info" then "alert alert-info"       # blue
+	      when "notice" then "alert alert-info"     # blue
+	      when "warning" then "alert alert-warning" # yellow
+	      when "alert" then "alert alert-warning"   # yellow
+	      when "danger" then "alert alert-danger"   # red
+	      when "error" then "alert alert-danger"    # red
+	    end
+	  end
+
 end
